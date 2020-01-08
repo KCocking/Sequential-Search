f3dfed7a5b8afaa6f0dc755f8817531433f0903c
Kasey Cocking <KCocking@my.gcu.edu>
2020-01-08 13:23:35 -0700

    added function main

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+/*
+sequential search:
+i = 0;
+while i < numElements;
+	if arr[i] == target
+		return i and quit
+	else
+		i++
+
+	//invariant: target is not in arr[0] -> arr[i-1]
+
+sorting algorithims:
+	selection sort:
+		searches a set for the largest int then moves it to the end
+		then repeats the process till the set is in smallest to largest order
+		17 4 8 14 7 9
+		4 8 14 7 9 17
+		4 8 7 9 14 17
+		4 8 7 9 14 17
+		4 7 8 9 14 17
+
+	bubble sort:
+		
+	insertion sort:
+
+*/
+
+
